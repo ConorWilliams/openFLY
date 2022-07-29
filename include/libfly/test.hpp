b3fa33c55115471a4319fe9279c8edc0fcf76489
ConorWilliams <conorwilliams@outlook.com>
2022-07-29 11:58:26 +0100

    dox generating

--- include/libfly/test.hpp
+++ include/libfly/test.hpp
@@ -1,6 +1,63 @@
 #pragma once
 
 /**
+ * @brief A small kitty.
+ *
+ */
+struct cat {
+  /**
+   * @brief stores the time of day $x = y$
+   *
+   */
+  int meow;
+};
+
+namespace fly {
+/**
+ * @brief an ugly cat.
+ */
+struct cat {
+  int meow;
+};
+
+/**
+ * @brief A function
+ *
+ * Inserting additional reStructuredText information. ds
+ *
+ *  A inline formula: \f$ f(x) = a + b \f$
+ *
+ * A display style formula: dddd
+ *
+ * @f[
+ * \int_a^b f(x) dx = F(b) - F(a)
+ * @f]
+ *
+ *
+ * Example usage:
+ * \verbatim embed:rst:leading-asterisk
+ * .. include:: ../examples/main.cpp
+ *    :code:
+ * \endverbatim
+ *
+ * @code
+ * char *buffer = new char[42];
+ * int charsAdded = sprintf(buffer, "Tabs are normally %d spaces\n", 8);
+ * @endcode
+ */
+inline constexpr void foo(){};
+
+namespace impl {
+/**
+ * @brief BAsic secreys
+ *
+ */
+struct impl_S {};
+} // namespace impl
+
+} // namespace fly
+
+/**
  * @brief A big noop.
  *
  * @tparam T Must be an int.
@@ -44,20 +101,20 @@ class Cpp {};
  * @tparam T Never an int.
  */
 template <typename T> struct builder {
-  public:
-    /** @brief An a */
-    int a;
-    int b; ///< An b.
+public:
+  /** @brief An a */
+  int a;
+  int b; ///< An b.
 
-    /**
-     * @brief Do "The Thing"!
-     */
-    void foo();
+  /**
+   * @brief Do "The Thing"!
+   */
+  void foo();
 
-  private:
-    /** @brief An a */
-    int c;
-    int d; ///< An b.
+private:
+  /** @brief An a */
+  int c;
+  int d; ///< An b.
 };
 
 } // namespace stuff

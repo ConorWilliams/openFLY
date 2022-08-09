b840681227dac793ca16f6c7a129c73162ed8cbd
ConorWilliams <conorwilliams@outlook.com>
2022-08-09 19:10:15 +0100

    box size

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,7 +1,34 @@
+#include <iostream>
 
+#include "libfly/io/gsd.hpp"
+#include "libfly/system/box.hpp"
+#include "libfly/system/boxes/orthorhombic.hpp"
+#include "libfly/utility/asserts.hpp"
+#include "libfly/utility/core.hpp"
+#include "libfly/utility/timeit.hpp"
 
 int main() {
   //
+
+  fly::io::FileGSD file("build/test.gsd", fly::io::read_write);
+
+  fly::system::Box box_read(2 * fly::Mat<double>::Identity(), fly::Arr<bool>::Constant(true));
+
+  file.load(0, box_read);
+
+  file.clear();
+
+  std::cout << "n " << file.n_frames() << std::endl;
+
+  fly::system::Box box(fly::Mat<double>::Identity(), fly::Arr<bool>::Constant(true));
+
+  ASSERT(box_read == box, "same?");
+
+  ASSERT(box.holding<fly::system::Orthorhombic>(), "");
+
+  fly::timeit("dump box 1", [&] { file.dump(box); });
+
+  std::cout << "n " << file.n_frames() << std::endl;
 
   return 0;
 }
--- include/libfly/io/gsd.hpp
+++ include/libfly/io/gsd.hpp
@@ -14,12 +14,18 @@
 
 // You should have received a copy of the GNU General Public License along with openFLY. If not, see <https://www.gnu.org/licenses/>.
 
+#include <memory>
+#include <string>
+#include <string_view>
+#include <utility>
+
+#include "libfly/system/box.hpp"
 #include "libfly/utility/core.hpp"
 
 /**
  * \file gsd.hpp
  *
- * @brief GSD file io support.
+ * @brief Binary IO support in the GSD format.
  *
  * \rst
  *
@@ -29,11 +35,85 @@
  * \endrst
  */
 
+/**
+ * @brief External type
+ */
+struct gsd_handle;
+
 namespace fly::io {
 
   /**
-   * @brief oops!
+   * @brief Flags for file permissions
    */
-  void test();
+  enum Flags {
+    read,        ///< Open with read only permissions.
+    read_write,  ///< Open with read and write permissions.
+    create       ///< Open with read and write permissions, overwrite existing files.
+  };
+
+  /**
+   * @brief file
+   *
+   */
+  class FileGSD {
+  public:
+    /**
+     * @brief Construct a new File G S D object
+     *
+     * @param fname
+     * @param flag
+     */
+    explicit FileGSD(std::string_view fname, Flags flag = read);
+
+    /**
+     * @brief yes
+     *
+     * @tparam Args
+     * @param args
+     */
+    template <typename... Args>
+    void dump(Args const &...args) {
+      (static_cast<void>(dump_impl(args)), ...);
+      commit_frame();
+    }
+
+    /**
+     * @brief no
+     *
+     * @tparam Args
+     */
+    template <typename... Args>
+    void load(int i, Args &&...args) const {
+      (static_cast<void>(load_impl(i, std::forward<Args>(args))), ...);
+    }
+
+    /**
+     * @brief plop
+     *
+     * @return int
+     */
+    int n_frames() const noexcept;
+
+    /**
+     * @brief Clear the current file.
+     *
+     * After truncating, a file will have no frames and no data chunks. The file size will be that of a newly created gsd file. The
+     * application, schema, and schema version metadata will be kept. Clear does not close and reopen the file, so it is suitable
+     * for writing restart files on Lustre file.
+     */
+    void clear();
+
+    ~FileGSD() noexcept;
+
+  private:
+    std::string m_fname;
+    std::unique_ptr<gsd_handle> m_handle;
+
+    void dump_impl(system::Box const &);
+
+    void load_impl(int i, system::Box &) const;
+
+    void commit_frame();
+  };
 
 }  // namespace fly::io

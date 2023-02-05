138b142bfceec344516cb0354582a4595c59f754
Conor Williams <conorwilliams@outlook.com>
2023-02-05 12:53:27 +0000

    kim energy

--- apps/kim.cpp
+++ apps/kim.cpp
@@ -1,22 +1,11 @@
 
 #include <fmt/core.h>
 
-#include <KIM_ChargeUnit.hpp>
-#include <KIM_FunctionTypes.hpp>
-#include <KIM_LogVerbosity.hpp>
-#include <KIM_ModelRoutineName.hpp>
-#include <KIM_SpeciesName.hpp>
-#include <KIM_TemperatureUnit.hpp>
-#include <KIM_TimeUnit.hpp>
 #include <cstddef>
 #include <exception>
 #include <nonstd/span.hpp>
 
-#include "KIM_SimulatorHeaders.hpp"
-#include "KIM_SupportedExtensions.hpp"
-
 //
-
 #include "libfly/env/catalogue.hpp"
 #include "libfly/io/gsd.hpp"
 #include "libfly/kinetic/basin.hpp"
@@ -26,6 +15,7 @@
 #include "libfly/neigh/list.hpp"
 #include "libfly/neigh/sort.hpp"
 #include "libfly/potential/EAM/data.hpp"
+#include "libfly/potential/KIM/kim.hpp"
 #include "libfly/potential/generic.hpp"
 #include "libfly/saddle//find.hpp"
 #include "libfly/saddle/dimer.hpp"
@@ -40,335 +30,6 @@
 #include "libfly/utility/core.hpp"
 #include "libfly/utility/lattice.hpp"
 #include "libfly/utility/random.hpp"
-
-namespace fly::potential {
-
-  class KIM_API {
-  public:
-    struct Options {
-      std::string model_name = "";  // The exact name of the KIM model
-    };
-
-    explicit KIM_API(Options const& opt, system::TypeMap<> const& map) : m_opt(opt), m_map(map.num_types()) {
-      int units_accepted = false;
-      int err = KIM::Model::Create(KIM::NUMBERING::zeroBased,
-                                   KIM::LENGTH_UNIT::A,
-                                   KIM::ENERGY_UNIT::eV,
-                                   KIM::CHARGE_UNIT::unused,
-                                   KIM::TEMPERATURE_UNIT::unused,
-                                   KIM::TIME_UNIT::unused,
-                                   m_opt.model_name,
-                                   &units_accepted,
-                                   &m_model);
-
-      fly::verify(!err, "Kim model not found");
-      fly::verify(units_accepted, "Kim model rejected our units");
-      fly::verify(m_model, "Model pointer not assigned");
-
-      // Check that we know about all required routines
-
-      int num_names = 0;
-
-      KIM::MODEL_ROUTINE_NAME::GetNumberOfModelRoutineNames(&num_names);
-
-      for (int i = 0; i < num_names; ++i) {
-        //
-        KIM::ModelRoutineName name;
-
-        if (KIM::MODEL_ROUTINE_NAME::GetModelRoutineName(i, &name)) {
-          throw fly::error("Unable to get ModelRoutineName");
-        }
-
-        int present = 0;
-        int required = 0;
-
-        if (m_model->IsRoutinePresent(name, &present, &required)) {
-          throw fly::error("Unable to get routine present/required.");
-        }
-
-        fmt::print("Model routine name \"{}\": present = {} and required = {}\n",
-                   name.ToString(),
-                   bool(present),
-                   bool(required));
-
-        if ((present == true) && (required == true)) {
-          using namespace KIM::MODEL_ROUTINE_NAME;
-          if (!((name == Create) || (name == ComputeArgumentsCreate) || (name == Compute) || (name == Refresh)
-                || (name == ComputeArgumentsDestroy) || (name == Destroy))) {
-            throw fly::error("Unknown Routine \"{}\" is required by model.", name.ToString());
-          }
-        }
-      }
-
-      // Print model units
-      KIM::LengthUnit lengthUnit;
-      KIM::EnergyUnit energyUnit;
-      KIM::ChargeUnit chargeUnit;
-      KIM::TemperatureUnit temperatureUnit;
-      KIM::TimeUnit timeUnit;
-
-      m_model->GetUnits(&lengthUnit, &energyUnit, &chargeUnit, &temperatureUnit, &timeUnit);
-
-      fmt::print("Length unit is \"{}\"\n", lengthUnit.ToString());
-      fmt::print("Energy unit is \"{}\"\n", energyUnit.ToString());
-      fmt::print("Charge unit is \"{}\"\n", chargeUnit.ToString());
-      fmt::print("Temperature unit is \"{}\"\n", temperatureUnit.ToString());
-      fmt::print("Time unit is \"{}\"\n", timeUnit.ToString());
-
-      // Check species
-
-      for (Eigen::Index i = 0; i < map.num_types(); i++) {
-        int supported;
-        int model_code;
-
-        KIM::SpeciesName name = std::string{
-            map.get(i, tp_),
-        };
-
-        if (m_model->GetSpeciesSupportAndCode(name, &supported, &model_code) || !supported) {
-          throw error(
-              "Species \"{}\" unsupported by KIM potential \"{}\"", name.ToString(), m_opt.model_name);
-        }
-
-        verify(model_code == i, "Species {} uses KIM code {} != {}", name.ToString(), model_code, i);
-      }
-
-      // Set up compute request
-
-      if (m_model->ComputeArgumentsCreate(&m_args)) {
-        throw error("Unable to create a m_args object");
-      }
-
-      fly::verify(m_args, "Compute arguments pointer not assigned");
-
-      // check compute arguments
-      int num_comp_names = 0;
-
-      namespace KAN = KIM::COMPUTE_ARGUMENT_NAME;
-
-      KAN::GetNumberOfComputeArgumentNames(&num_comp_names);
-
-      for (int i = 0; i < num_comp_names; ++i) {
-        KIM::ComputeArgumentName name;
-        KIM::SupportStatus supportStatus;
-        KAN::GetComputeArgumentName(i, &name);
-        KIM::DataType dataType;
-        KAN::GetComputeArgumentDataType(name, &dataType);
-
-        if (m_args->GetArgumentSupportStatus(name, &supportStatus)) {
-          throw error("unable to get ComputeArgument SupportStatus");
-        }
-
-        fmt::print("Compute argument \"{}\" is of type \"{}\" and has support status \"{}\"\n",
-                   name.ToString(),
-                   dataType.ToString(),
-                   supportStatus.ToString());
-
-        // Everything must be optional
-        if (supportStatus == KIM::SUPPORT_STATUS::required) {
-          throw error("unsupported required ComputeArgument: \"{}\"", name.ToString());
-        }
-
-        // Must have energy and forces as optional arguments
-        if ((name == KAN::partialEnergy) || (name == KAN::partialForces)) {
-          if (supportStatus != KIM::SUPPORT_STATUS::optional) {
-            throw error("Energy or Forces are not available");
-          }
-        }
-      }
-
-      // Check compute callbacks
-      int num_call_names = 0;
-      KIM::COMPUTE_CALLBACK_NAME::GetNumberOfComputeCallbackNames(&num_call_names);
-
-      for (int i = 0; i < num_call_names; ++i) {
-        KIM::ComputeCallbackName name;
-        KIM::COMPUTE_CALLBACK_NAME::GetComputeCallbackName(i, &name);
-        KIM::SupportStatus supportStatus;
-        m_args->GetCallbackSupportStatus(name, &supportStatus);
-
-        fmt::print("Callback \"{}\" has support status \"{}\"\n", name.ToString(), supportStatus.ToString());
-
-        // Cannot handle any "required" callbacks
-        if (supportStatus == KIM::SUPPORT_STATUS::required) {
-          throw error("unsupported required ComputeCallback");
-        }
-      }
-
-      // Check supported extensions, if any
-      int present = 0;
-
-      if (m_model->IsRoutinePresent(KIM::MODEL_ROUTINE_NAME::Extension, &present, NULL)) {
-        throw error("Unable to get Extension present/required.");
-      }
-
-      if (present) {
-        KIM::SupportedExtensions supportedExtensions;
-
-        if (m_model->Extension(KIM_SUPPORTED_EXTENSIONS_ID, &supportedExtensions)) {
-          throw error("Error returned from KIM::Model::Extension().");
-        }
-      }
-
-      // Check neighbour list requirements
-
-      int const* modelWillNotRequestNeighborsOfNoncontributingParticles;
-      double const* cutoff_cluster_model;
-      int number_of_neighbor_lists;
-
-      m_model->GetNeighborListPointers(&number_of_neighbor_lists,
-                                       &cutoff_cluster_model,
-                                       &modelWillNotRequestNeighborsOfNoncontributingParticles);
-
-      fmt::print("Model will request {} neighbour lists\n", number_of_neighbor_lists);
-
-      for (int i = 0; i < number_of_neighbor_lists; ++i) {
-        fmt::print("\t List {} has cut-off {} and will-not-request-neigh-of-non-contributing={}\n",
-                   i,
-                   cutoff_cluster_model[i],
-                   bool(modelWillNotRequestNeighborsOfNoncontributingParticles[i]));
-
-        if (!modelWillNotRequestNeighborsOfNoncontributingParticles[i]) {
-          throw error("Cannot provide neigh-lists for this KIM potential");
-        }
-      }
-      // ignoring hints from here on...
-      if (number_of_neighbor_lists != 1) {
-        throw error("Kim model needs {} neighbour lists!", number_of_neighbor_lists);
-      }
-
-      // We're compatible with the model. Let's do it.
-    }
-
-    ~KIM_API() noexcept {
-      if (m_model) {
-        if (m_args) {
-          if (m_model->ComputeArgumentsDestroy(&m_args)) {
-            throw error("KIM failed to clean-up model");
-          }
-          m_args = nullptr;
-        }
-
-        KIM::Model::Destroy(&m_model);
-
-        m_model = nullptr;
-      } else {
-        verify(m_args == nullptr, "KIM_API in invalid state for clean-up");
-      }
-    }
-
-    /**
-     * @brief Get this potentials cut-off radius.
-     *
-     * This is the maximum distance two atom can interact. The neighbour::List passed to the other functions
-     * should be configured with a cut-off equal or greater than this.
-     */
-    auto r_cut() const noexcept -> double {
-      double influence_distance_cluster_model = 0;
-      m_model->GetInfluenceDistance(&influence_distance_cluster_model);
-      return influence_distance_cluster_model;
-    }
-
-    /**
-     * @brief Compute potential energy gradient.
-     *
-     * Assumes the neighbour list are ready, force on frozen atoms will be zero.
-     *
-     * @param in Per-atom TypeID's and Frozen properties
-     * @param out Potential gradient written to this.
-     * @param nl Neighbour list (in ready state i.e. neigh::List::update() or neigh::List::rebuild()
-     * called).
-     * @param threads Number of openMP threads to use.
-     */
-    auto gradient(system::SoA<PotentialGradient&> out,
-                  system::SoA<TypeID const&, Frozen const&> in,
-                  neigh::List const& nl,
-                  int) -> void {
-      //
-
-      verify(out.size() == in.size(), "size mismatch! {} != {}", out.size(), in.size());
-
-      int num_plus_ghosts = nl.m_num_plus_ghosts;
-
-      if (m_kim_io.size() != num_plus_ghosts) {
-        m_kim_io.destructive_resize(num_plus_ghosts);
-      }
-
-      for (Eigen::Index i = 0; i < num_plus_ghosts; i++) {
-        m_kim_io(KIM_code{}, i) = safe_cast<int>(in(id_, nl.image_to_real(i)));
-        m_kim_io(KIM_contrib{}, i) = i < nl.size();
-      }
-
-      namespace KAN = KIM::COMPUTE_ARGUMENT_NAME;
-
-      if (m_args->SetArgumentPointer(KAN::numberOfParticles, &num_plus_ghosts)
-          || m_args->SetArgumentPointer(KAN::particleSpeciesCodes, m_kim_io[KIM_code{}].data())
-          || m_args->SetArgumentPointer(KAN::particleContributing, m_kim_io[KIM_contrib{}].data())
-          || m_args->SetArgumentPointer(KAN::coordinates, nl.m_atoms[r_].data())
-          || m_args->SetArgumentPointer(KAN::partialEnergy, static_cast<double*>(nullptr))
-          || m_args->SetArgumentPointer(KAN::partialForces, m_kim_io[KIM_force{}].data())) {
-        throw("KIM_API_set_argument_pointer");
-      }
-
-      if (m_args->SetCallbackPointer(KIM::COMPUTE_CALLBACK_NAME::GetNeighborList,
-                                     KIM::LANGUAGE_NAME::cpp,
-                                     reinterpret_cast<void (*)()>(&neigh::List::get_neigh),
-                                     const_cast<neigh::List*>(&nl)  // C api has no
-                                     )) {
-        throw error("KIM_API_set_callback");
-      }
-
-      m_args->PushLogVerbosity(KIM::LOG_VERBOSITY::debug);
-
-      int res = 0;
-
-      m_args->AreAllRequiredArgumentsAndCallbacksPresent(&res);
-
-      verify(res, "Failed to set all required arguments and callbacks!");
-
-      if (m_model->Compute(m_args)) {
-        throw error("KIM_API_compute");
-      }
-
-      out[g_] = 0;
-
-      for (Eigen::Index i = 0; i < num_plus_ghosts; ++i) {
-        if (auto j = nl.image_to_real(i); !in(fzn_, j)) {
-          out(g_, j) -= m_kim_io(KIM_force{}, i);
-        }
-      }
-    }
-
-    /**
-     * @brief Compute the potential energy.
-     *
-     * Assumes the neighbour list are ready, ignores contributions from the frozen atoms.
-     *
-     * @param in Per-atom TypeID's and Frozen properties.
-     * @param nl Neighbour list (in ready state i.e. neigh::List::update() or neigh::List::rebuild() called).
-     * @param threads Number of openMP threads to use.
-     * @return double The potential energy of the system of atoms.
-     */
-    auto energy(system::SoA<TypeID const&, Frozen const&> in, neigh::List const& nl, int threads = 1)
-        -> double;
-
-  private:
-    Options m_opt;
-
-    struct KIM_code : system::Property<int> {};
-    struct KIM_contrib : system::Property<int> {};
-    struct KIM_energy : system::Property<double> {};
-    struct KIM_force : system::Property<double, 3> {};
-
-    system::TypeMap<KIM_code> m_map;
-
-    system::SoA<KIM_code, KIM_contrib, KIM_energy, KIM_force> m_kim_io;
-
-    KIM::Model* m_model = nullptr;
-    KIM::ComputeArguments* m_args = nullptr;
-  };
-
-}  // namespace fly::potential
 
 using namespace fly;
 
@@ -401,7 +62,7 @@ system::Supercell<system::TypeMap<>, Position, Frozen, T...> bcc_iron_motif(doub
 int main(int, const char**) {
   //
 
-  system::Supercell perfect = motif_to_lattice(bcc_iron_motif(), {16, 16, 16});
+  system::Supercell perfect = motif_to_lattice(bcc_iron_motif(), {6, 6, 6});
 
   system::Supercell cell = remove_atoms(perfect, {1, 3});
 
@@ -426,9 +87,11 @@ int main(int, const char**) {
 
     nl.rebuild(cell, omp_get_max_threads());
 
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 5; i++) {
       timeit("KIM    ", [&] { model.gradient(grad_kim, cell, nl, 1); });
     }
+
+    fmt::print("energy={:.10f}\n", model.energy(cell, nl, 1));
   }
 
   potential::Generic pot{
@@ -444,9 +107,11 @@ int main(int, const char**) {
 
     nl.rebuild(cell, omp_get_max_threads());
 
-    for (int i = 0; i < 2; i++) {
-      timeit("Generic", [&] { pot.gradient(grad_gen, cell, nl, 4); });
+    for (int i = 0; i < 5; i++) {
+      timeit("Generic", [&] { pot.gradient(grad_gen, cell, nl, 1); });
     }
+
+    fmt::print("energy={:.10f}\n", pot.energy(cell, nl, 1));
   }
 
   // print a comparison of the gradients
---
+++ src/potential/KIM/kim.cpp
@@ -0,0 +1,361 @@
+// Copyright © 2020-2022 Conor Williams <conorwilliams@outlook.com>
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// This file is part of openFLY.
+
+// OpenFLY is free software: you can redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+
+// OpenFLY is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+
+// You should have received a copy of the GNU General Public License along with openFLY. If not, see
+// <https://www.gnu.org/licenses/>.
+
+#include <algorithm>
+#include <libfly/potential/KIM/kim.hpp>
+//
+#include "KIM_SimulatorHeaders.hpp"
+#include "KIM_SupportedExtensions.hpp"
+#include "libfly/utility/core.hpp"
+
+namespace fly::potential {
+
+  KIM_API::KIM_API(Options const& opt, system::TypeMap<> const& map) : m_opt(opt), m_map(map.num_types()) {
+    //
+    int units_accepted = false;
+    int err = KIM::Model::Create(KIM::NUMBERING::zeroBased,
+                                 KIM::LENGTH_UNIT::A,
+                                 KIM::ENERGY_UNIT::eV,
+                                 KIM::CHARGE_UNIT::unused,
+                                 KIM::TEMPERATURE_UNIT::unused,
+                                 KIM::TIME_UNIT::unused,
+                                 m_opt.model_name,
+                                 &units_accepted,
+                                 &m_model);
+
+    fly::verify(!err, "Kim model not found");
+    fly::verify(units_accepted, "Kim model rejected our units");
+
+    ASSERT(m_model, "Model pointer not assigned", 0);
+
+    // Check that we know about all required routines
+
+    int num_names = 0;
+
+    KIM::MODEL_ROUTINE_NAME::GetNumberOfModelRoutineNames(&num_names);
+
+    for (int i = 0; i < num_names; ++i) {
+      //
+      KIM::ModelRoutineName name;
+
+      if (KIM::MODEL_ROUTINE_NAME::GetModelRoutineName(i, &name)) {
+        throw fly::error("Unable to get ModelRoutineName");
+      }
+
+      int present = 0;
+      int required = 0;
+
+      if (m_model->IsRoutinePresent(name, &present, &required)) {
+        throw fly::error("Unable to get routine present/required.");
+      }
+
+      dprint(m_opt.debug,
+             "KIM: Model routine name \"{}\": present = {} and required = {}\n",
+             name.ToString(),
+             bool(present),
+             bool(required));
+
+      if (present == true && required == true) {
+        //
+        std::array names = {
+            KIM::MODEL_ROUTINE_NAME::Create,
+            KIM::MODEL_ROUTINE_NAME::ComputeArgumentsCreate,
+            KIM::MODEL_ROUTINE_NAME::Compute,
+            KIM::MODEL_ROUTINE_NAME::Refresh,
+            KIM::MODEL_ROUTINE_NAME::ComputeArgumentsDestroy,
+            KIM::MODEL_ROUTINE_NAME::Destroy,
+        };
+
+        if (std::find(names.begin(), names.end(), name) == names.end()) {
+          throw fly::error("Unknown Routine \"{}\" is required by model.", name.ToString());
+        }
+      }
+    }
+
+    // Print model units
+    KIM::LengthUnit lengthUnit;
+    KIM::EnergyUnit energyUnit;
+    KIM::ChargeUnit chargeUnit;
+    KIM::TemperatureUnit temperatureUnit;
+    KIM::TimeUnit timeUnit;
+
+    m_model->GetUnits(&lengthUnit, &energyUnit, &chargeUnit, &temperatureUnit, &timeUnit);
+
+    dprint(m_opt.debug, "KIM: Length unit is \"{}\"\n", lengthUnit.ToString());
+    dprint(m_opt.debug, "KIM: Energy unit is \"{}\"\n", energyUnit.ToString());
+    dprint(m_opt.debug, "KIM: Charge unit is \"{}\"\n", chargeUnit.ToString());
+    dprint(m_opt.debug, "KIM: Temperature unit is \"{}\"\n", temperatureUnit.ToString());
+    dprint(m_opt.debug, "KIM: Time unit is \"{}\"\n", timeUnit.ToString());
+
+    // Check species
+
+    for (Eigen::Index i = 0; i < map.num_types(); i++) {
+      int supported;
+      int model_code;
+
+      auto j = safe_cast<std::uint32_t>(i);
+
+      KIM::SpeciesName name = std::string{map.get(j, tp_)};
+
+      if (m_model->GetSpeciesSupportAndCode(name, &supported, &model_code) || !supported) {
+        throw error("Species \"{}\" unsupported by KIM potential \"{}\"", name.ToString(), m_opt.model_name);
+      }
+
+      m_map.set(j, tp_, map.get(j, tp_));
+      m_map.set(j, KIM_code{}, model_code);
+    }
+
+    // Set up compute request
+
+    if (m_model->ComputeArgumentsCreate(&m_args)) {
+      throw error("Unable to create a m_args object");
+    }
+
+    ASSERT(m_args, "Compute arguments pointer not assigned", 0);
+
+    // Check compute arguments.
+    int num_comp_names = 0;
+
+    namespace KAN = KIM::COMPUTE_ARGUMENT_NAME;
+
+    KAN::GetNumberOfComputeArgumentNames(&num_comp_names);
+
+    for (int i = 0; i < num_comp_names; ++i) {
+      KIM::ComputeArgumentName name;
+      KIM::SupportStatus supportStatus;
+      KAN::GetComputeArgumentName(i, &name);
+      KIM::DataType dataType;
+      KAN::GetComputeArgumentDataType(name, &dataType);
+
+      if (m_args->GetArgumentSupportStatus(name, &supportStatus)) {
+        throw error("unable to get ComputeArgument SupportStatus");
+      }
+
+      dprint(m_opt.debug,
+             "KIM: Compute argument \"{}\" is of type \"{}\" and has support status \"{}\"\n",
+             name.ToString(),
+             dataType.ToString(),
+             supportStatus.ToString());
+
+      // Everything must be optional
+      if (supportStatus == KIM::SUPPORT_STATUS::required) {
+        throw error("Unsupported required ComputeArgument: \"{}\"", name.ToString());
+      }
+
+      // Must have energy and forces as optional arguments
+      if ((name == KAN::partialEnergy) || (name == KAN::partialForces)) {
+        if (supportStatus != KIM::SUPPORT_STATUS::optional) {
+          throw error("KIM - Energy or Forces are not available");
+        }
+      }
+    }
+
+    // Check compute callbacks
+    int num_call_names = 0;
+    KIM::COMPUTE_CALLBACK_NAME::GetNumberOfComputeCallbackNames(&num_call_names);
+
+    for (int i = 0; i < num_call_names; ++i) {
+      KIM::ComputeCallbackName name;
+      KIM::COMPUTE_CALLBACK_NAME::GetComputeCallbackName(i, &name);
+      KIM::SupportStatus supportStatus;
+      m_args->GetCallbackSupportStatus(name, &supportStatus);
+
+      dprint(m_opt.debug,
+             "KIM: Callback \"{}\" has support status \"{}\"\n",
+             name.ToString(),
+             supportStatus.ToString());
+
+      // Cannot handle any "required" callbacks
+      if (supportStatus == KIM::SUPPORT_STATUS::required) {
+        throw error("unsupported required ComputeCallback");
+      }
+    }
+
+    // Check supported extensions, if any
+    int present = 0;
+
+    if (m_model->IsRoutinePresent(KIM::MODEL_ROUTINE_NAME::Extension, &present, NULL)) {
+      throw error("Unable to get Extension present/required.");
+    }
+
+    if (present) {
+      KIM::SupportedExtensions supportedExtensions;
+
+      if (m_model->Extension(KIM_SUPPORTED_EXTENSIONS_ID, &supportedExtensions)) {
+        throw error("Error returned from KIM::Model::Extension().");
+      }
+    }
+
+    // Check neighbour list requirements
+
+    int const* modelWillNotRequestNeighborsOfNoncontributingParticles;
+    double const* cutoff_cluster_model;
+    int number_of_neighbor_lists;
+
+    m_model->GetNeighborListPointers(&number_of_neighbor_lists,
+                                     &cutoff_cluster_model,
+                                     &modelWillNotRequestNeighborsOfNoncontributingParticles);
+
+    dprint(m_opt.debug, "KIM: Model will request {} neighbour lists\n", number_of_neighbor_lists);
+
+    for (int i = 0; i < number_of_neighbor_lists; ++i) {
+      dprint(m_opt.debug,
+             "\tKIM: List {} has cut-off {} and will-not-request-neigh-of-non-contributing={}\n",
+             i,
+             cutoff_cluster_model[i],
+             bool(modelWillNotRequestNeighborsOfNoncontributingParticles[i]));
+
+      if (!modelWillNotRequestNeighborsOfNoncontributingParticles[i]) {
+        throw error("Cannot provide ghost neigh-lists for this KIM potential");
+      }
+    }
+
+    if (number_of_neighbor_lists != 1) {
+      throw error("Kim model needs {} neighbour lists!", number_of_neighbor_lists);
+    }
+
+    // We're compatible with the model. Let's do it.
+  }
+
+  KIM_API::~KIM_API() noexcept {
+    if (m_model) {
+      if (m_args) {
+        if (m_model->ComputeArgumentsDestroy(&m_args)) {
+          throw error("KIM failed to clean-up compute arguments");
+        }
+      }
+      KIM::Model::Destroy(&m_model);
+    }
+  }
+
+  auto KIM_API::r_cut() const noexcept -> double {
+    double influence_distance_cluster_model = 0;
+    m_model->GetInfluenceDistance(&influence_distance_cluster_model);
+    return influence_distance_cluster_model;
+  }
+
+  double KIM_API::energy(system::SoA<TypeID const&, Frozen const&> in, neigh::List const& nl, int) {
+    //
+    int num_plus_ghosts = safe_cast<int>(nl.m_num_plus_ghosts);
+
+    if (m_kim_io.size() != num_plus_ghosts) {
+      m_kim_io.destructive_resize(num_plus_ghosts);
+    }
+
+    for (Eigen::Index i = 0; i < num_plus_ghosts; i++) {
+      m_kim_io(KIM_code{}, i) = m_map.get(in(id_, nl.image_to_real(i)), KIM_code{});
+      m_kim_io(KIM_contrib{}, i) = i < nl.size();
+    }
+
+    namespace KAN = KIM::COMPUTE_ARGUMENT_NAME;
+
+    if (m_args->SetArgumentPointer(KAN::numberOfParticles, &num_plus_ghosts)
+        || m_args->SetArgumentPointer(KAN::particleSpeciesCodes, m_kim_io[KIM_code{}].data())
+        || m_args->SetArgumentPointer(KAN::particleContributing, m_kim_io[KIM_contrib{}].data())
+        || m_args->SetArgumentPointer(KAN::coordinates, nl.m_atoms[r_].data())
+        || m_args->SetArgumentPointer(KAN::partialForces, static_cast<double*>(nullptr))
+        || m_args->SetArgumentPointer(KAN::partialEnergy, m_kim_io[KIM_energy{}].data())) {
+      throw("KIM_API_set_argument_pointer");
+    }
+
+    if (m_args->SetCallbackPointer(KIM::COMPUTE_CALLBACK_NAME::GetNeighborList,
+                                   KIM::LANGUAGE_NAME::cpp,
+                                   reinterpret_cast<void (*)()>(&neigh::List::get_neigh),
+                                   const_cast<neigh::List*>(&nl)  // C api has no const-correctness
+                                   )) {
+      throw error("KIM_API_set_callback");
+    }
+
+    int res = 0;
+
+    m_args->AreAllRequiredArgumentsAndCallbacksPresent(&res);
+
+    verify(res, "Failed to set all required arguments and callbacks!");
+
+    if (m_model->Compute(m_args)) {
+      throw error("KIM_API_compute");
+    }
+
+    // Sum up the partial energies
+
+    double energy = 0;
+
+    for (int i = 0; i < num_plus_ghosts; i++) {
+      energy += m_kim_io(KIM_energy{}, i);
+    }
+
+    return energy;
+  }
+
+  auto KIM_API::gradient(system::SoA<PotentialGradient&> out,
+                         system::SoA<TypeID const&, Frozen const&> in,
+                         neigh::List const& nl,
+                         int) -> void {
+    //
+
+    verify(out.size() == in.size(), "size mismatch! {} != {}", out.size(), in.size());
+
+    int num_plus_ghosts = safe_cast<int>(nl.m_num_plus_ghosts);
+
+    if (m_kim_io.size() != num_plus_ghosts) {
+      m_kim_io.destructive_resize(num_plus_ghosts);
+    }
+
+    for (Eigen::Index i = 0; i < num_plus_ghosts; i++) {
+      m_kim_io(KIM_code{}, i) = m_map.get(in(id_, nl.image_to_real(i)), KIM_code{});
+      m_kim_io(KIM_contrib{}, i) = i < nl.size();
+    }
+
+    namespace KAN = KIM::COMPUTE_ARGUMENT_NAME;
+
+    if (m_args->SetArgumentPointer(KAN::numberOfParticles, &num_plus_ghosts)
+        || m_args->SetArgumentPointer(KAN::particleSpeciesCodes, m_kim_io[KIM_code{}].data())
+        || m_args->SetArgumentPointer(KAN::particleContributing, m_kim_io[KIM_contrib{}].data())
+        || m_args->SetArgumentPointer(KAN::coordinates, nl.m_atoms[r_].data())
+        || m_args->SetArgumentPointer(KAN::partialEnergy, static_cast<double*>(nullptr))
+        || m_args->SetArgumentPointer(KAN::partialForces, m_kim_io[KIM_force{}].data())) {
+      throw("KIM_API_set_argument_pointer");
+    }
+
+    if (m_args->SetCallbackPointer(KIM::COMPUTE_CALLBACK_NAME::GetNeighborList,
+                                   KIM::LANGUAGE_NAME::cpp,
+                                   reinterpret_cast<void (*)()>(&neigh::List::get_neigh),
+                                   const_cast<neigh::List*>(&nl)  // C api has no const-correctness
+                                   )) {
+      throw error("KIM_API_set_callback");
+    }
+
+    int res = 0;
+
+    m_args->AreAllRequiredArgumentsAndCallbacksPresent(&res);
+
+    verify(res, "Failed to set all required arguments and callbacks!");
+
+    if (m_model->Compute(m_args)) {
+      throw error("KIM_API_compute");
+    }
+
+    out[g_] = 0;
+
+    for (Eigen::Index i = 0; i < num_plus_ghosts; ++i) {
+      if (auto j = nl.image_to_real(i); !in(fzn_, j)) {
+        out(g_, j) -= m_kim_io(KIM_force{}, i);
+      }
+    }
+  }
+
+}  // namespace fly::potential
